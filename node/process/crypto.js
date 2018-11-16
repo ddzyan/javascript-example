b4bfb74537e7db18709443fb595cdf5f70932acf
丁东照 <dingdz@sanlogic.com>
2018-11-16 21:23:34 +0800

    添加 aes-128-ecb 的加密和解密

---
+++ node/process/crypto.js
@@ -0,0 +1,54 @@
+const crypto = require('crypto');
+
+
+const aesutil = {};
+/**
+ * aes加密
+ * @param data 待加密内容
+ * @param key 必须为16位私钥
+ * @returns {string}
+ */
+aesutil.encryption = function (data, key) {
+    if (!data && key.length == 16) {
+        return "";
+    }
+    console.log('Original cleartext: ' + data);
+    const algorithm = 'aes-128-ecb';
+    const clearEncoding = 'utf8';
+    const iv = "";
+    const cipherEncoding = 'base64';
+    const cipher = crypto.createCipheriv(algorithm, key, iv); // 创建加密对象
+
+    const cipherChunks = [];
+    cipherChunks.push(cipher.update(data, clearEncoding, cipherEncoding));
+    cipherChunks.push(cipher.final(cipherEncoding));
+    console.log(`${cipherEncoding} ciphertext ${cipherChunks.join('')} \n`);
+
+    aesutil.decryption(cipherChunks.join(''), key);
+}
+
+/**
+ * aes解密
+ * @param data 待解密内容
+ * @param key 必须为16位私钥
+ * @returns {string}
+ */
+aesutil.decryption = function (data, key, iv) {
+    if (!data && key.length == 16) {
+        return "";
+    }
+    iv = iv || "";
+    var clearEncoding = 'utf8';
+    var cipherEncoding = 'base64';
+    var algorithm = 'aes-128-ecb';
+    var cipherChunks = [];
+    var decipher = crypto.createDecipheriv(algorithm, key, iv); // 创建解密对象
+    cipherChunks.push(decipher.update(data, cipherEncoding, clearEncoding));
+    cipherChunks.push(decipher.final(clearEncoding));
+    console.log("decryption:", cipherChunks.join(''));
+}
+
+const beforString = "j5cFtKCEWftVTAEjuo1K7PvEjmMqeKTikn";
+const key = "8r9mPAsWm3N&abcd"; // key 必须为16位字符串
+
+aesutil.encryption(beforString, key);
--- node/process/signal.js
+++ node/process/signal.js
@@ -1,11 +1,4 @@
 process.stdin.resume(); //这句话是为了不让控制台推出
 process.on('SIGINT', function () { //SIGINT这个信号是系统默认信号，代表信号中断，就是ctrl+c
   console.log('Got SIGINT.  Press Control-D to exit.');
-  
 });
-
-process.on('uncaughtException', (err) => {
-    console.log(err);
-});
-
-nonexistentFunc();
