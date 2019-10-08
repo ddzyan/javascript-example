403e15217bc41d86f55228a0b645a1bb2f982d2b
dingdz <ddzyan@163.com>
2019-10-08 14:36:46 +0800

    补充异步编程方法demo

--- 异步编程方法/generator.js
+++ 异步编程方法/generator.js
@@ -1,21 +1,57 @@
-function* foo(x) {
-  var y = 2 * (yield x + 1);
-  var z = yield y / 3;
-  return x + y + z;
-}
 /**
- * 结果分析
+ * js 异步编程实现方式: Generator 函数
+ *
+ * 使用 Generator 封装异步任务
+ * 函数将在 yield 阶段暂停，返回一个可以遍历的对象
+ * 每次执行对象的 next() 方法，将执行 yield 方法，并将结果返回
  */
-const a = foo(5);
-console.log("a.next() :", a.next()); // Object{value:6, done:false}
-console.log("a.next() :", a.next()); // Object{value:NaN, done:false}
-console.log("a.next() :", a.next()); // Object{value:NaN, done:true}
+
+function timeout(name) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("generator step", name);
+    }, 1000);
+  });
+}
 
 /**
- * 结果分析
- *
+ * 模拟异步任务
+ * 方法将在 yield 暂停，执行语法糖后面的任务
+ * 执行完毕后，才会进行执行下面代码
+ * 执行顺序为：
+ * 1. 第一次执行 next() ,执行到 yield timeout(),
+ * 2. 第二次执行 next() ,传入的参数将为 yield timeout() 的结果赋值 result，再执行接下来的打印输出
  */
-const b = foo(5);
-console.log("b.next() :", b.next()); // { value:6, done:false }
-console.log("b.next(12) :", b.next(12)); // { value:8, done:false }
-console.log("b.next(13) :", b.next(13)); // { value:42, done:true }
+function* generator() {
+  const result = yield timeout();
+  console.log(
+    "将 next() 传入的值做为 yield timeout() 结果,赋值给 result ,值为:",
+    result
+  );
+}
+
+const g = generator();
+const result1 = g.next();
+/**
+ * result1 的值为一个 Promise 对象，所以需要用 then 方法获得异步结果
+ */
+console.log("g.next() 第一次执行结果 :", result1);
+result1.value
+  .then(function(data) {
+    console.log("获取 result1 Promise 对象结果", data);
+    return data;
+  })
+  .then(function(data) {
+    const result2 = g.next(data);
+    console.log("g.next() 第二次执行结果 :", result2);
+  });
+
+/** 
+ * 打印输出
+start
+g.next() 第一次执行结果 : { value: Promise { <pending> }, done: false }
+result1 Promise 对象返回结果 generator step
+result : 2
+end
+g.next() 第二次执行结果 : { value: undefined, done: true }
+ */
